3986eee1195ccc2d2810ab12935ddc3db81da614
Yajush-afk <yajush24606@gmail.com>
2026-02-27 08:40:02 +0530

    feat: enhance `remove` command to clear all tasks when no index is provided, improve default help message, and format `list` output with a box.

--- taskcli/main.py
+++ taskcli/main.py
@@ -23,14 +23,17 @@ def main():
         "remove",
         help="remove a task"
     )
-    remove_parser.add_argument("index", type=int, help="index of task to remove")
+    remove_parser.add_argument("index", type=int, nargs="?", help="index of task to remove")
     subparser.add_parser("list", help="list all tasks")
     update_parser = subparser.add_parser("update", help="update a task")
     update_parser.add_argument("index", type=int, help="index of task to update")
     update_parser.add_argument("title", type=str, help="new title for the task")
     args = parser.parse_args()
     if args.command is None:
-        parser.print_help()
+        print(f"""taskcli - A basic python-cli for todo lists.
+        \nFunctionalities: 1. add, 2. list, 3. remove, 4. update
+        \nUse -h, --help for more information.""")
+
         sys.exit(1)
     if args.command == "add":
         handle_add(args.title)
@@ -49,6 +52,25 @@ def handle_add(title):
 
 def handle_remove(index):
     tasks = load_tasks()
+    if index is None:
+        try:
+            choice = input("NOTE: taskcli remove {index} may remove the task you desire, remove command if run without any index CLEAR the list.\nDo you wish to proceed [y/N]: ")
+        except KeyboardInterrupt:
+            print("\nOperation cancelled.")
+            sys.exit(0)
+            
+        if choice.strip().lower() == 'y':
+            save_tasks([])
+            print("List cleared.")
+            return
+        elif choice.strip().lower() == 'n':
+            print("enter index with remove command, use taskcli list to check index number.")
+            return
+        else:
+            print("Invalid choice, enter y or n.")
+            handle_remove(None)
+            return
+
     if index < 1 or index > len(tasks):
         print(f"Invalid task index: {index}, check using 'taskcli list'")
         sys.exit(1)
@@ -61,8 +83,15 @@ def handle_list():
     if not tasks:
         print("nothing to list, create a task using 'taskcli add <task_name>'")
         return
-    for i, task in enumerate(tasks, 1):
-        print(f"{i}. {task}")
+    
+    lines = [f"{i}. {task}" for i, task in enumerate(tasks, 1)]
+    max_len = max(len(line) for line in lines)
+    width = max_len + 2
+    
+    print("┌" + "─" * width + "┐")
+    for line in lines:
+        print(f"│ {line.ljust(max_len)} │")
+    print("└" + "─" * width + "┘")
 
 def handle_update(index, title):
     tasks = load_tasks()
